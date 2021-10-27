601612832f33704d1abf751bf46e9e9df58e8b17
hnisar01 <72013214+hnisar01@users.noreply.github.com>
2021-10-27 12:40:47 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mvcmoviesreal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
